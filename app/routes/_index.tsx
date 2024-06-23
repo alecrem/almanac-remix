9caab64252cafbcd6308bdee735952635a861851
Alejandro Cremades <ale@alecrem.com>
2024-06-24 00:19:51 +0900

    Use timezone from env var to get date from the desired timezone

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,8 +4,23 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const timezone = process.env.TIMEZONE;
+const options: Intl.DateTimeFormatOptions = {
+  timeZone: timezone,
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+};
+const formatter = new Intl.DateTimeFormat(undefined, options);
+const getDateStringForTargetTimezone = (): string => {
+  const [rawMonth, rawDay, year] = formatter.format(new Date()).split("/");
+  const month = Number(rawMonth) > 9 ? rawMonth : `0${rawMonth}`;
+  const day = Number(rawDay) > 9 ? rawDay : `0${rawDay}`;
+  return `${year}-${month}-${day}T00:00:00.000Z`;
+};
+
 export const loader = async () => {
-  const date = `${new Date().toISOString().substring(0, 10)}T00:00:00.000Z`;
+  const date = getDateStringForTargetTimezone();
   return json({
     events: await prisma.almanac.findFirst({
       where: {
