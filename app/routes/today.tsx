07282197fd97884fdd9886fb75fcc02336f85d4d
Alejandro Cremades <ale@alecrem.com>
2024-06-25 00:10:02 +0900

    Add tailwindcss

--- app/routes/today.tsx
+++ app/routes/today.tsx
@@ -1,5 +1,10 @@
 import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
