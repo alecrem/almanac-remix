a92f95c5a9d71d458641276f7efdd4b29e1cb388
Alejandro Cremades <ale@alecrem.com>
2024-07-15 00:29:29 +0900

    Turn the front page into the iframe-only page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,10 +22,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const { url } = useLoaderData<typeof loader>();
   const iframeUrl = `${url}today`;
-  const iframeCode = `<iframe title="${siteTitle}" src="${iframeUrl}" height="380" width="380" frameBorder="0" scrolling="no" style="border: 0; overflow: hidden;" />`;
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>{siteTitle}</h1>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <iframe
           title={siteTitle}
@@ -36,13 +34,6 @@ export default function Index() {
           frameBorder={0}
           scrolling="no"
         />
-        <textarea
-          rows={5}
-          style={{
-            width: "372px",
-          }}
-          value={iframeCode}
-        ></textarea>
       </div>
     </div>
   );
