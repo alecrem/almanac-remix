e3021b796a784e2dfae44af79eaba0cdf25164b3
Alejandro Cremades <ale@alecrem.com>
2024-09-11 20:56:30 +0900

    Use semantic HTML tags on DateBox component

--- app/components/datebox/index.tsx
+++ app/components/datebox/index.tsx
@@ -6,16 +6,16 @@ type Props = {
 
 const DateBox = (props: Props) => {
   return (
-    <div className={"bg-neutral-300 p-8 aspect-square min-w-96 max-w-96"}>
+    <article className={"bg-neutral-300 p-8 aspect-square min-w-96 max-w-96"}>
       <div className={"bg-white p-8 aspect-square"}>
         <div className={"text-black align-middle min-h-max h-max"}>
-          <div className={"text-black text-5xl font-bold text-right"}>
+          <h2 className={"text-black text-5xl font-bold text-right"}>
             {props.date}/{props.month}
-          </div>
-          <div className={"pt-16 text-lg font-light"}>{props.event}</div>
+          </h2>
+          <p className={"pt-16 text-lg font-light"}>{props.event}</p>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
