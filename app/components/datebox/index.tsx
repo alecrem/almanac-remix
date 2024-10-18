d53f8193c6d31434c646f4a4d35cc51a19fb2c0a
Gerard Girbes Berges <knute@Air-de-Gerard.home>
2024-10-18 12:35:44 +0200

    Replaced Tailwind class names with custom plain CSS found in reset.css and remixflex.css.

--- app/components/datebox/index.tsx
+++ app/components/datebox/index.tsx
@@ -1,3 +1,6 @@
+import "../css/reset.css";
+import "../css/remixflex.css";
+
 type Props = {
   date: string;
   month: string;
@@ -6,14 +9,12 @@ type Props = {
 
 const DateBox = (props: Props) => {
   return (
-    <article className={"bg-neutral-300 p-8 aspect-square min-w-96 max-w-96"}>
-      <div className={"bg-white p-8 aspect-square"}>
-        <div className={"text-black align-middle min-h-max h-max"}>
-          <h2 className={"text-black text-5xl font-bold text-right"}>
+    <article className={"layout"}>
+      <div>
+          <h2>
             {props.date}/{props.month}
           </h2>
-          <p className={"mt-16 text-lg font-light"}>{props.event}</p>
-        </div>
+          <p>{props.event}</p>
       </div>
     </article>
   );
